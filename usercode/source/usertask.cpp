4e62f4e7608f2f1fcdd6cae918e612cc26842706
makabaka <mahaoxuan666@sjtu.edu.cn>
2025-11-02 17:15:35 +0800

    feat:消息队列test

--- usercode/source/usertask.cpp
+++ usercode/source/usertask.cpp
@@ -2,7 +2,12 @@
 
 #include "cmsis_os.h"
 #include"cmsis_os2.h"
-uint32_t count=0;
+uint32_t send=0;
+uint32_t recv=0;
+
+osMessageQueueId_t test_queue_handle;
+osMessageQueueAttr_t test_queue_atrributes ={.name="test_queue"};
+
 
 osThreadId_t testTaskHandle;
 constexpr osThreadAttr_t testTask_attributes = {
@@ -11,15 +16,32 @@ constexpr osThreadAttr_t testTask_attributes = {
     .priority=osPriorityBelowNormal
 };
 
-void test_task(void *) {
+[[noreturn]] void test_task(void *) {
     while (true) {
         const auto tick=osKernelGetTickCount();
-        ++count;
+        send+=1;
+        osMessageQueuePut(test_queue_handle,&send,0,0);
         osDelayUntil(tick+1);
     }
 }
 
+osThreadId_t test1TaskHandle;
+constexpr osThreadAttr_t test1Task_attributes = {
+    .name="test1Task",
+    .stack_size = 256*4,
+    .priority=osPriorityBelowNormal
+};
+
+[[noreturn]] void test1_task(void *) {
+    while (true) {
+        osMessageQueueGet(test_queue_handle,&recv,0,0);
+    }
+}
+
 void user_tasks_init() {
+    test_queue_handle = osMessageQueueNew(10,sizeof(uint32_t),&test_queue_atrributes);
     testTaskHandle=osThreadNew(test_task,nullptr,&testTask_attributes);
+    test1TaskHandle=osThreadNew(test1_task,nullptr,&test1Task_attributes);
+
 
 }
