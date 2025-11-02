f182ffd2cf0650934519d3f3bd06bb679fc4f1e0
makabaka <mahaoxuan666@sjtu.edu.cn>
2025-11-02 14:19:04 +0800

    feat:初始模版

---
+++ MDK-ARM/freertos_test/usertask.d
@@ -0,0 +1,9 @@
+freertos_test/usertask.o: ..\usercode\source\usertask.cpp \
+  ..\usercode\include\usertask.h \
+  ..\Middlewares\Third_Party\FreeRTOS\Source\CMSIS_RTOS_V2\cmsis_os2.h \
+  D:\keil5\core\ARM\ARMCLANG\Bin\..\include\libcxx\stdint.h \
+  D:\keil5\core\ARM\ARMCLANG\Bin\..\include\libcxx\__config \
+  D:\keil5\core\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  D:\keil5\core\ARM\ARMCLANG\Bin\..\include\libcxx\stddef.h \
+  D:\keil5\core\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  D:\keil5\core\ARM\ARMCLANG\Bin\..\include\libcxx\__nullptr
---
+++ usercode/include/usertask.h
@@ -0,0 +1,19 @@
+//
+// Created by ROG STRIX on 2025/11/1.
+//
+#include"cmsis_os2.h"
+#ifndef FREERTOS_TEST_USERTASK_H
+#define FREERTOS_TEST_USERTASK_H
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+    void user_task_init();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //FREERTOS_TEST_USERTASK_H
